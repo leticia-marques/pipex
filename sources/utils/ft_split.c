9909e71063c0ddb60dd7f6715ae9c70b1a5f34f1
Letícia Marques <leticia.marquess@protonmail.com>
2022-01-13 15:59:31 -0300

    fs

--- sources/utils/ft_split.c
+++ sources/utils/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lemarque <lemarque@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:49:46 by lemarque          #+#    #+#             */
-/*   Updated: 2022/01/13 13:36:31 by lemarque         ###   ########.fr       */
+/*   Updated: 2022/01/13 15:58:30 by lemarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,33 +62,42 @@ static int	get_word_size(char const *str, char c)
 	return (i);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**get_string(char **arr_words, const char *s, char c, size_t *i)
 {
-	char	**arr_words;
-	size_t	word_size;
-	size_t	i;
 	int		count;
+	int		word_size;
 	size_t	words_count;
 
-	words_count = get_words_number(s,c);
+	words_count = get_words_number(s, c);
 	count = 0;
-	i = 0;
-	arr_words = malloc(sizeof(char *) * (words_count + 1));
-	if (!arr_words)
-		return (NULL);
 	while (*s)
 	{
 		count = set_c(&s, &c, count);
 		word_size = get_word_size(s, c);
-		if (*s != c && *s != '\'' && i < words_count)
+		if (*s != c && *s != '\'' && *i < words_count)
 		{
-			arr_words[i] = ft_substr(s, 0, word_size);
-			if (!arr_words[i])
+			arr_words[*i] = ft_substr(s, 0, word_size);
+			if (!arr_words[*i])
 				return (NULL);
 			s += word_size;
-			i++;
+			*i += 1;
 		}
 	}
+	return (arr_words);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**arr_words;
+	size_t	i;
+	size_t	words_count;
+
+	words_count = get_words_number(s, c);
+	i = 0;
+	arr_words = malloc(sizeof(char *) * (words_count + 1));
+	if (!arr_words)
+		return (NULL);
+	get_string(arr_words, s, c, &i);
 	arr_words[i] = NULL;
 	return (arr_words);
 }
